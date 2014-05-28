8261aee5db090a39ca363697969ac10f42e601c5
Tomasz Finc <tomasz@gmail.com>
2014-05-28 01:47:23 -0700

    Printing out whole first page rather then just the items that mismatched

--- windygaming.rb
+++ windygaming.rb
@@ -18,10 +18,10 @@ if found_stock
     if stock["1"] != buffer[0]["title"] or
         stock["2"] != buffer[2]["title"] or
         stock["3"] != buffer[4]["title"]
-            print "New Items Found!\n"
-            puts buffer[0]["title"], windy_url + buffer[0]["href"]
-            puts buffer[2]["title"], windy_url + buffer[2]["href"]
-            puts buffer[4]["title"], windy_url + buffer[4]["href"]
+            puts "New Items Found!\n\n"
+            for i in 0..11
+                puts buffer[i*2]["title"], windy_url + buffer[i*2]["href"], "\n"
+            end
     end
 end
 
