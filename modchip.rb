11fa58f2c82337946849421159c56320926267a7
Tomasz Finc <tomasz@gmail.com>
2014-05-28 02:24:23 -0700

    Fixing rubocop and ruby-lint warnings

--- modchip.rb
+++ modchip.rb
@@ -2,12 +2,13 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-item_url = "http://www.segastyle.com/store/index.php?route=product/product&product_id=76"
+item_url =
+    'http://www.segastyle.com/store/index.php?
+        route=product/product&product_id=76'
 
-page = Nokogiri::HTML(open(item_url)) 
+page = Nokogiri::HTML(open(item_url))
 
-buffer = page.css('div.description span:nth-child(3)')          [0].next_sibling.text.strip.gsub(/\n  +/, "\n")
+buffer = page.css('div.description span:nth-child(3)')[0]
+    .next_sibling.text.strip.gsub(/\n  +/, "\n")
 
-if buffer.include? "In Stock"
-    puts item_url
-end
+puts item_url if buffer.include? 'In Stock'
--- windygaming.rb
+++ windygaming.rb
@@ -3,33 +3,32 @@ require 'nokogiri'
 require 'open-uri'
 require 'json'
 
-stock_file = "stock"
-windy_url = "http://www.windygaming.com"
+stock_file = 'stock'
+windy_url = 'http://www.windygaming.com'
+windy_new = 'http://www.windygaming.com/collections/newest-items'
 
-if File.file?(stock_file)
-    stock = JSON.parse(IO.read(stock_file))
-end
+stock = JSON.parse(IO.read(stock_file)) if File.file?(stock_file)
 
-page = Nokogiri::HTML(open("http://www.windygaming.com/collections/newest-items")) 
+page = Nokogiri::HTML(open(windy_new))
 
 buffer = page.css('div.product-grid-item a')
 if stock
-    if stock["1"] != buffer[0]["title"] or
-        stock["2"] != buffer[2]["title"] or
-        stock["3"] != buffer[4]["title"]
-            puts "New Items Found!\n\n"
-            for i in 0..11
-                puts buffer[i*2]["title"], windy_url + buffer[i*2]["href"], "\n"
-            end
+  if stock['1'] != buffer[0]['title'] ||
+     stock['2'] != buffer[2]['title'] ||
+     stock['3'] != buffer[4]['title']
+    puts "New Items Found!\n\n"
+    (0..11).each do |i|
+      puts buffer[i * 2]['title'], windy_url + buffer[i * 2]['href'], "\n"
     end
+  end
 end
 
-my_hash = {:"1" => buffer[0]["title"],
-           :"2" => buffer[2]["title"],
-           :"3" => buffer[4]["title"]}
+my_hash = { :"1" => buffer[0]['title'],
+            :"2" => buffer[2]['title'],
+            :"3" => buffer[4]['title'] }
 
 json = JSON.pretty_generate(my_hash)
 
-File.open(stock_file, "w") do |f|
-    f.write(json)
+File.open(stock_file, 'w') do |f|
+  f.write(json)
 end
