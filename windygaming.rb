b92053290ffebf0b192a72991dadf1ad4dde67f4
Tomasz Finc <tomasz@gmail.com>
2014-05-30 15:23:41 -0700

    Checking agains\'t all new items now rather then top three. Setting default match threshold

--- windygaming.rb
+++ windygaming.rb
@@ -28,6 +28,7 @@ require 'json'
 stock_file = 'stock'
 windy_url = 'http://www.windygaming.com'
 windy_new = '/collections/newest-items'
+found = 0
 
 stock = JSON.parse(IO.read(stock_file)) if File.file?(stock_file)
 
@@ -36,21 +37,26 @@ page = Nokogiri::HTML(open(windy_url + windy_new))
 buffer = page.css('div.product-grid-item a')
 
 if stock
-  if stock['1'] != buffer[0]['title'] &&
-     stock['2'] != buffer[2]['title'] &&
-     stock['3'] != buffer[4]['title']
+  (0..10).each do |i|
+    if stock[i * 2] != buffer[i * 2]['title'] # HACK; should be stock[i]
+      found += 1
+    end
+  end
+  if found >= 6
     puts "New Items Found!\n\n"
-    (0..11).each do |i|
+    (0..10).each do |i|
       puts buffer[i * 2]['title'], windy_url + buffer[i * 2]['href'], "\n"
     end
   end
 end
 
-my_hash = { :"1" => buffer[0]['title'],
-            :"2" => buffer[2]['title'],
-            :"3" => buffer[4]['title'] }
+my_array = []
 
-json = JSON.pretty_generate(my_hash)
+(0..20).each do |i|
+  my_array[i] = buffer[i]['title']
+end
+
+json = JSON.generate(my_array)
 
 File.open(stock_file, 'w') do |f|
   f.write(json)
